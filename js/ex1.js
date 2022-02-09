70bd4e8cd2e82f350d9da4938db3e8e0e64f81df
Thy Huynh <khanhhthyy99@gmail.com>
2022-02-08 21:26:49 -0800

    ver 2.0

--- js/ex1.js
+++ js/ex1.js
@@ -1,4 +1,26 @@
 /* Tutorial 4
    Example 1 JavaScript code
 */
-console.log("Huynh's Output from Tutorial 4 Example 1");
+
+let firstName = prompt("Please enter your first name: ");
+let numUnits = prompt("Enter the number of units completed (>= 0): ");
+
+if (Number(numUnits) >= 0) {
+   //console.log("valid input");
+   units = Number(numUnits);
+   let grade;
+   if (units <= 30) {
+      grade = "Freshman";
+   } else if (units <= 60) {
+      grade = "Sophomore";
+   } else if (units <= 90) {
+      grade = "Junior";
+   } else {
+      grade = "Senior";
+   }
+   // Print output
+   console.log(`Hello ${firstName}`);
+   console.log(`Your grade standing is ${grade}`);
+} else {
+   console.log(`${firstName} you entered ${numUnits}.  You must enter a number >= 0`) 
+};
--- js/ex2.js
+++ js/ex2.js
@@ -1,4 +1,40 @@
 /* Tutorial 4
    Example 2 JavaScript code
 */
-console.log("Huynh's Output from Tutorial 4 Example 2");
+
+/* Tutorial 4
+   Example 1 JavaScript code
+*/
+
+const DayEntered = prompt("Please enter a day of the week: ");
+switch(DayEntered) {
+   case "sun":
+      console.log("The following day is Monday");
+      break; 
+   case "mon":
+      console.log("The following day is Tuesday");
+      break;
+   case "tue":
+      console.log("The following day is Wednesday");
+      break;
+   case "wed":
+      console.log("The following day is Thursday");
+      break;
+   case "thu":
+      console.log("The following day is Friday");
+      break;
+   case "fri":
+      console.log("The following day is Saturday");
+      break;
+   case "sat":
+      console.log("The following day is Sunday");
+      break;
+   case "sun":
+      console.log("The following day is Monday");
+      break;
+   default: 
+   console.log("Not a valid day. Please re-enter.");
+      
+}
+
+
